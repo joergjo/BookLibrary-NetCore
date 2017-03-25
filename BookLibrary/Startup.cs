2791059061b81d5ebce38db83382f01a895d9516
Jörg Jooss <joerg@joergjooss.de>
2017-03-25 16:34:46 +0100

    Replaced deprecated AddUserScrets().

--- BookLibrary/Startup.cs
+++ BookLibrary/Startup.cs
@@ -25,7 +25,7 @@ namespace BookLibrary
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                builder.AddUserSecrets<Startup>();
             }
 
             Configuration = builder.Build();
