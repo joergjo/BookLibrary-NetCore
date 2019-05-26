333e69b1786fe6cc7f40825dfa2448947d427299
Jörg Jooss <joerg@joergjooss.de>
2019-05-26 20:57:29 +0200

    Fixed bug in EpochDateTimeConverter. Code style update in BookRepository.

--- src/BookLibrary/Common/EpochDateTimeConverter.cs
+++ src/BookLibrary/Common/EpochDateTimeConverter.cs
@@ -10,7 +10,7 @@ namespace BookLibrary.Common
         {
             if (reader.TokenType == JsonToken.Integer)
             {
-                long ticks = (long) reader.Value;
+                long ticks = (long)reader.Value;
                 var epoch = NewEpoch();
                 var date = epoch.AddMilliseconds(ticks);
                 return date;
@@ -22,25 +22,19 @@ namespace BookLibrary.Common
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            if (value is DateTime)
+            if (value is DateTime date)
             {
                 var epoch = NewEpoch();
-                var timeSpan = ((DateTime) value) - epoch;
-                if (timeSpan.TotalSeconds >= 0)
-                {
-                    long ticks = (long) timeSpan.TotalMilliseconds;
-                    writer.WriteValue(ticks);
-                    return;
-                }
-
-                throw new JsonSerializationException(
-                    $"Unexpected epoch time {timeSpan.Seconds}.");
+                var timeSpan = date - epoch;
+                long ticks = (long)timeSpan.TotalMilliseconds;
+                writer.WriteValue(ticks);
+                return;
             }
 
             throw new JsonSerializationException(
                 $"Unexpected type {value?.GetType()}.");
         }
 
-        private static DateTime NewEpoch() => new DateTime(1970, 1, 1);
+        protected static DateTime NewEpoch() => new DateTime(1970, 1, 1);
     }
 }
